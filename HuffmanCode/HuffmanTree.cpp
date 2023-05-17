4c3089e22ce1bdc8c993f0fc7ae6eb239b2d5679
alex-h-vierra <112529361+alex-h-vierra@users.noreply.github.com>
2023-05-17 16:30:16 -0700

    Update HuffmanTree.cpp

--- HuffmanCode/HuffmanTree.cpp
+++ HuffmanCode/HuffmanTree.cpp
@@ -40,46 +40,24 @@ void HuffmanTree::printBinary(vector<char> bytes, std::ostream &out) const {
     for (char byte: bytes) {
         printBits(byte, out);
         out << "-";
+        out << endl;
     }
 
 }
 
 string HuffmanTree::getCode(char letter) const {
-    string code = "";
-    int j = 0;
-    BinaryNode *temp = root;
-    for (int i = 0; i <= temp->element.size(); i++) {
-        if (temp->element[i] - letter == 0) {
+    int check = 0;
+    string code;
+    for (int i = 0; i < root->element.size(); i++) {
+        if (letter == root->element[i]) {
+            check = 1;
             break;
         }
-        if (i == temp->element.size()) {
-            return code + "not found";
-        }
     }
-    while (true) {
-        if (temp->left != nullptr) {
-            for (int i = 0; i < temp->left->element[i]; i++) {
-                if (temp->left->element[i] - letter == 0) {
-                    temp = temp->left;
-                    code = code + '0';
-                    if (temp->left == nullptr && temp->right == nullptr) {
-                        return code;
-                    }
-                }
-            }
-        }
-        if (temp->right != nullptr) {
-            for (int i = 0; i < temp->right->element[i]; i++) {
-                if (temp->right->element[i] - letter == 0) {
-                    temp = temp->right;
-                    code = code + '1';
-                    if (temp->left == nullptr && temp->right == nullptr) {
-                        return code;
-                    }
-                }
-            }
-        }
+    if (check == 1) {
+        code = codeLookup.at(letter);
     }
+    return code;
 }
 
 void HuffmanTree::makeEmpty(BinaryNode *&t) {
@@ -100,7 +78,7 @@ void HuffmanTree::printTree(BinaryNode *node, std::ostream &out) const {
         printTree(node->right, out);
     }
     if (node->left == nullptr && node->right == nullptr) {
-        out << node->element << ": " << node->frequency << "|";
+        out << node->element << ": " << node->frequency << "|" << endl;
     }
 }
 
@@ -112,22 +90,22 @@ void HuffmanTree::printCodes(BinaryNode *node, std::ostream &out, string code) c
         printCodes(node->right, out, code + '1');
     }
     if (node->left == nullptr && node->right == nullptr) {
-        out << node->element << ": " << code << "|";
+        out << node->element << ": " << code << "|" << endl;
     }
 }
 
-
+/*The idea of savetree is to save our letter with a key being the letter
+ * and pairing that key is a binary code that is create by recursively going
+ * down the tree*/
 void HuffmanTree::saveTree(BinaryNode *current,
-                           string code) { //we pass in the root with an empty string
-    // need to write code    //left will be tagged as 1
-    //Right will be tagged as 0
+                           string code) {
     if (current->left != nullptr) {
         saveTree(current->left, code + '0');
     }
     if (current->right != nullptr) {
         saveTree(current->right, code + '1');
     }
-    if (current->left == nullptr && current->right == nullptr){
+    if (current->left == nullptr && current->right == nullptr) {
         codeLookup[current->element[0]] = code;
     }
 }
@@ -136,6 +114,18 @@ void HuffmanTree::saveTree(BinaryNode *current,
 void HuffmanTree::saveTree(std::ostream &compressedFileStream) {
     //TODO Project 2 need to write code
     // calls recursive function
+    int mapSize = codeLookup.size();
+    compressedFileStream << mapSize << ":";
+    /*this loop makes sure that End of File character isn't added
+     * into the file to cause a segmentation fault*/
+    for (auto pair: codeLookup) {
+        if (pair.first == 0) {
+        }
+        else {
+            //Pair.first = key , Pair.second = binary code
+            compressedFileStream << pair.first << " = " << pair.second << ':' << endl;
+        }
+    }
 }
 
 
@@ -148,17 +138,26 @@ void HuffmanTree::rebuildTree(BinaryNode *node, string element, string codedRout
 void HuffmanTree::rebuildTree(ifstream &compressedFile) {
     // read info from file
     // use info to build tree
-
+    string codedRoute;
+    codedRoute = compressedFile.good();
+    rebuildTree(root, string(), codedRoute);
     //TODO Project 2 need to write code
     // calls recursive function
 }
 
+/*
+ * The Idea behind this function is to create
+ * our set up adding into the priority queue
+ * as well as combine what's in our priority queue*/
 HuffmanTree::BinaryNode *HuffmanTree::buildTree(string frequencyText) {
     priority_queue<HuffmanTree::BinaryNode *,
             vector<HuffmanTree::BinaryNode *>,
             compareBinaryNodes> nodes;
     BinaryNode *thing1 = nullptr;
     BinaryNode *thing2 = nullptr;
+    /*These two loops help combine and add up the
+     * amount of how many are in the text
+     * with the array size of 128 being the max ascii value*/
     char fArray[128]{0};
     for (int i = 0; i < frequencyText.size(); i++) {
         fArray[frequencyText[i]]++;
@@ -169,6 +168,8 @@ HuffmanTree::BinaryNode *HuffmanTree::buildTree(string frequencyText) {
             nodes.push(temp);
         }
     }
+    /*This while loop is combining all the frequency text together
+     * to make our huffman tree based on the priority queue*/
     while (nodes.size() > 1) {
         thing1 = nodes.top();
         nodes.pop();
@@ -214,19 +215,25 @@ void HuffmanTree::printTree(std::ostream &out) const {
 void HuffmanTree::makeEmpty() {
     makeEmpty(root);
 }
-
+/*The idea of decode is to take a encoded string and roll down
+ * the Huffman tree to find the encoded letter*/
 string HuffmanTree::decode(vector<char> encodedBytes) {
     string decoded;
     int iterator = 0;
     BinaryNode *nodeIterator = root;
     while (iterator < encodedBytes.size()) {
-        if (encodedBytes[iterator] == '0') {
+        if (nodeIterator->left == nullptr || nodeIterator->right == nullptr) {
+            decoded = decoded + nodeIterator->element;
+            nodeIterator = root;
+            iterator = iterator + 1;
+        }
+        else if (encodedBytes[iterator] == '0') {
             nodeIterator = nodeIterator->left;
             iterator = iterator + 1;
         }
         else if (encodedBytes[iterator] == '1') {
             nodeIterator = nodeIterator->right;
-            iterator = iterator + 1;
+            iterator += 1;
         }
         else {
             decoded = decoded + nodeIterator->element;
@@ -237,90 +244,93 @@ string HuffmanTree::decode(vector<char> encodedBytes) {
     return decoded;
 }
 
+/*The idea of this function is to take a letter and change its bit to
+ * and push back its bit mask to a vector*/
 vector<char> HuffmanTree::encode(string stringToEncode) {
     stringToEncode.push_back(
             EOFCharacter); // needed when encoding message for file I/O
     vector<char> encoded;
-    string temp = "";
-    for (int i = 0; i < stringToEncode.size(); i++) {
-        for (int j = 0; j < root->element.size(); j++) {
-            if (root->element[j] - stringToEncode[i] == 0) {
-                temp = temp + getCode(stringToEncode[i]);
-                temp = temp + EOFCharacter;
-                break;
-            }
+//    encoded.push_back(0);
+    string temp;
+    /*This for loop is in charge of creating a string based
+     * on the letters binary code from the codeLookup*/
+    for (char ch: stringToEncode) {
+        for (char i: codeLookup[ch]) {
+            temp.push_back(i);
         }
     }
-    for (int i = 0; i < temp.size(); i++) {
-        encoded.push_back(temp[i]);
+    int bitPosition = 7;
+    int iterator = 0;
+    char ch = 0;
+    /*This while loop is en charge of changing the bit mask when encountering
+     * a 1 or 0 and turning off or on that bit position*/
+    while (temp.size() > iterator) {
+        if (bitPosition < 0) {
+            encoded.push_back(ch);
+            ch++;
+            bitPosition = 7;
+        }
+        if (temp[iterator] == '1') {
+            setBit(ch, bitPosition);
+        }
+        bitPosition--;
+        iterator++;
     }
     return encoded;
+
 }
 
+/*The idea on an UncompressFile is to rebuild and decode from the compressed file*/
 void HuffmanTree::uncompressFile(string compressedFileName,
                                  string uncompressedToFileName) {
     //TODO Project 2 need to write code
-
+    ifstream compressedFile(compressedFileName, ios::out | ios::binary);
+    ofstream fOut(uncompressedToFileName, ios::out | ios::binary);
+    if (compressedFile.fail()) {
+        cout << "failed to open: " << compressedFileName << " in uncompressFile" << endl;
+    }
+    ostringstream temp;
+    temp << compressedFile.rdbuf();
+    const string &s = temp.str();
+    vector<char> encodedVec;
+    string unCompressedString;
+    /*The idea for this for loop is to push back the 1's and 0's
+     * and for setting up to decode vector we will pass in*/
+    for (int i = 8; i < s.size(); i++) {
+        if (s[i] == '1' || s[i] == '0' || s[i] == ':') {
+            encodedVec.push_back(s[i]);
+        }
+    }
+    unCompressedString = decode(encodedVec);
+    /*This loop is outputting the decoded string into a file from
+     * UnCompressedString*/
+    for (char i: unCompressedString) {
+        fOut << i;
+    }
+    fOut.close();
+    compressedFile.close();
     // NOTE: when opening the compressedFile, you need to open in
-    //  binary mode for reading..hmmm..why is that?
+    // binary mode for reading..hmmm..why is that?
 }
 
+/*The idea of the CompressFIle is to save our bit masks into a file
+ * that we set in our encoded function*/
 void HuffmanTree::compressFile(string compressToFileName,
                                string uncompressedFileName, bool buildNewTree) {
     //TODO Project 2 need to write code
-
-    // NOTE: when opening the compressedFile, you need to open in
-    //  binary mode for writing..hmmm..why is that?
+    ifstream compressedFile(uncompressedFileName, ios::out | ios::binary);
+    ofstream fOut(compressToFileName, ios::out | ios::binary);
+    if (compressedFile.fail()) {
+        cout << "File failed to open: " << uncompressedFileName << "in compressFile"
+             << endl;
+    }
+    ostringstream temp;
+    temp << compressedFile.rdbuf();
+    const string &s = temp.str();
+    vector<char> compressed = encode(s);
+    /*SaveTree is the idea to output the saved encode bit mask
+     * to a file*/
+    saveTree(fOut);
+    compressedFile.close();
+    fOut.close();
 }
-/*
- * "C:\Users\alexv\CS 233\HuffmanCode\cmake-build-debug\HuffmanCode.exe"
-priority_queue test area:
-Top: 100
-New Top: 10
-10
-100
-101
-111
-1011
-1110
-1111
-
-
-unordered_map test area:
-c has: compiler
-d = 10010
-c = changed
-b = ballon
-a = apple
-Number of items stored in map: 4
-
-
-
-Test 1
-
-
-Building the tree from: HHHHEELLLLLLLOO WOOOOORRLLLLLLDP
-
-
-printTree:
-L: 13|E: 2|R: 2|H: 4|D: 1|W: 1| : 1|P: 1|O: 7|
-
-printCodes:
-L: 0|E: 1000|R: 1001|H: 101|D: 11000|W: 11001| : 11010|P: 11011|O: 111|
-
-
-Code L :0
-Code D :11000
-
-
-Test 2
-
-L: 0|E: 1000|R: 1001|H: 101|D: 11000|W: 11001| : 11010|P: 11011|O: 111|
-10001100-00001100-10001100-00000000-10001100-00001100-00001100-00001100-00000000-00001100-00000000-10001100-10001100-00001100-10001100-10001100-00000000-
-HELP:
-
-Press any key to continue . . . k
-
-
-Process finished with exit code 0
-*/
